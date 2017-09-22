06614dc88c017c5bc54be1e593ea32d002fe580f
11372273 <11372273@TAFT-CL334>
2017-09-22 14:40:26 +0800

    Initial Commit

---
+++ MyBookstore/MyBookstore/Models/PublishersModels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyBookstore.Models
+{
+    public class PublishersModels
+    {
+        public int ID { get; set; }
+
+    
+        public string Name { get; set; }
+
+    }
+}
